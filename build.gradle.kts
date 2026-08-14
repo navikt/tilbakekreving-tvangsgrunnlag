f8b8fe4f630997857d50ca7996007bc953ffeed0
Karl Amdam <karl.magnus.haddal.amdam@nav.no>
2026-08-14 08:00:45 +0200

    Legger til Gradle, Kotlin og Ktor

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+
+plugins {
+    kotlin("jvm") version "2.4.10"
+    id("io.ktor.plugin") version "3.5.2"
+}
+val logbackVersion = "1.6.1"
+
+
+group = "no.nav"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+dependencies {
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.netty)
+    implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+}
